a1d3fce1b84f8eae6693c1f4faef56bb2568cb71
dholi <comunidade@dholi.dev>
2020-10-19 17:08:13 -0300

    Tradução em Espanhol

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.0.1
+## xx/xx/2020
+
+1. [](#new)
+    * Tradução para Espanhol: Argentina, Chile, Colômbia, México, Panamá e Peru.
+    
 # 1.0.0
 ## 24/08/2020
 
--- src/module-payment/view/frontend/web/js/cash.js
+++ src/module-payment/view/frontend/web/js/cash.js
@@ -1,1 +1,1 @@
-define(["Dholi_Payment/js/payment"],function(a){return a.extend({defaults:{},initialize:function(){this._super();let self=this},initObservable:function(){this._super().observe([]);return this},getInstructions:function(){return window.checkoutConfig.payment[this.getCode()].instructions}})});
+define(["Dholi_Payment/js/payment"],function(a){return a.extend({defaults:{},initialize:function(){this._super()},initObservable:function(){this._super().observe([]);return this},getInstructions:function(){return window.checkoutConfig.payment[this.getCode()].instructions}})});
