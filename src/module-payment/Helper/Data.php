0ea5c83a47ef197a9f6a13e6b81e1ab8e0164a39
dholi <comunidade@dholi.dev>
2020-12-04 16:29:00 -0300

    PayU Latam

--- src/module-payment/Helper/Data.php
+++ src/module-payment/Helper/Data.php
@@ -6,7 +6,7 @@
 * @category     Dholi
 * @package      Modulo Payment Core
 * @copyright    Copyright (c) 2020 dholi (https://www.dholi.dev)
-* @version      1.0.0
+* @version      1.0.1
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
