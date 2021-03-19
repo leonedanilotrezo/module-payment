cee565c84d164abafc96543219c9205e0dbed007
dholi <comunidade@dholi.dev>
2021-03-19 16:50:27 -0300

    Version 1.1.0

--- src/module-payment/Setup/InstallData.php
+++ src/module-payment/Setup/InstallData.php
@@ -6,7 +6,7 @@
 * @category     Dholi
 * @package      Modulo Payment Core
 * @copyright    Copyright (c) 2020 dholi (https://www.dholi.dev)
-* @version      1.1.1
+* @version      1.1.0
 * @license      https://opensource.org/licenses/OSL-3.0
 * @license      https://opensource.org/licenses/AFL-3.0
 *
